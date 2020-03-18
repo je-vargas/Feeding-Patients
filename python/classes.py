a626fb30661342ba06358b392f605a0f49b3573a
Juan Vargas S <jv.esalamanca@gmail.com>
2020-03-18 22:32:11 +0000

    ..

--- python/classes.py
+++ python/classes.py
@@ -7,18 +7,15 @@ GRV = 3
 ISSUES = 4
 
 class Patient:
-
-    diagnosis_list = []
-    day_record_list = []
-    hr_data_list = []
-    data_counter = 0
-
+    
     def __init__(self, row_passed):
         self.__risk = row_passed[1]
         self.__age = row_passed[2]
         self.__current_grv = row_passed[3]
         self.__weight = row_passed[4]
         self.__target_grv = None
+        self.__data = list()
+        self.__diagosis = list()
 
     # ------- SETTER 
     def set_target_grv(self, target_grv):
@@ -28,18 +25,17 @@ class Patient:
         self.__current_grv = current_grv
 
     def set_daily_diagnosis(self, end_of_day_diagnosis):
-       diagnosis_list.append(end_of_day_diagnosis)
+       self.__diagosis.append(end_of_day_diagnosis)
 
-    def set_24hr_data(self, hourly_row):
-        hr_data_list[self.data_counter] = hourly_row
-        self.data_counter += 1
-
+    def set_data(self, data_array):
+        self.__data = data_array
+        
     def set_risk(self, risk):
         self.__risk = risk
         
     # ------- GETTER
     def get_daily_assessment(self):
-        return self.diagnosis
+        return self.__diagosis
 
     def get_target_grv(self):
         return self.__target_grv
@@ -50,11 +46,8 @@ class Patient:
     def get_risk(self):
         return self.__risk
 
-    def get_day_record(self):
-        return self.day_record
-
-    def get_24hr_datas(self):
-        return self.hr_data
+    def get_data(self):
+        return self.__data
 
 
 
