15c135d290feb0451abe2399723d1dd48471a0af
Juan Vargas S <jv.esalamanca@gmail.com>
2020-04-23 01:06:00 +0100

    moved main function to main.py and need to fix high risk patients

--- python/functions.py
+++ python/functions.py
@@ -482,19 +482,7 @@ def complete_sort(patient_lists, array_to_sort):
        print(str(patient_lists[i]) + " = " + str(array_to_sort[i]))
     
 
-if __name__ == "__main__":
 
-    patient_objects = [Patient() for i in range(len(CSV_PATHS))]
-    order_patient_files = ["Patient A1", "Patient A2", "Patient A3", "Patient B1", "Patient B2", "Patient B3", "Patient B4", "Patient B5", "Patient B6", "Patient B7"]
-    end_day_diagnosis = list()
-
-    # loop takes all the patients objects with csv to populate data
-    for i in range(len(CSV_PATHS)):
-        start_of_patient_diagnosis(CSV_PATHS[i], patient_objects[i])
-        end_day_diagnosis.append(patient_objects[i].get_week_diagnosis())
-
-    day_to_day_diagnosis_print(patient_objects, order_patient_files) # prints all patiets end of day diagnosis sequencially as required
-    complete_sort(order_patient_files, end_day_diagnosis)    
     
     
     
--- python/main.py
+++ python/main.py
@@ -1,10 +1,20 @@
-from functions import read_file
-from classes import Patient
+from functions import *
+from classes import *
 
 patient1_path = "/Users/juanestebanvargassalamanca/Desktop/Desktop – Juan’s MacBook Pro/UNI/Computer_Science /2nd_Year/Algorithm_DataStructures_(Python) /ASSIGNMENT_2/patients_csv/PATIENT DATA - PATIENT B1.csv"
 
 
 if __name__ == "__main__":
-    print ("reading patient row")
-    read_file(patient1_path, Patient)
+
+    patient_objects = [Patient() for i in range(len(CSV_PATHS))]
+    order_patient_files = ["Patient A1", "Patient A2", "Patient A3", "Patient B1", "Patient B2", "Patient B3", "Patient B4", "Patient B5", "Patient B6", "Patient B7"]
+    end_day_diagnosis = list()
+   
+    for i in range(len(CSV_PATHS)):
+        start_of_patient_diagnosis(CSV_PATHS[i], patient_objects[i])
+        end_day_diagnosis.append(patient_objects[i].get_week_diagnosis())
+
+    day_to_day_diagnosis_print(patient_objects, order_patient_files) # prints all patiets end of day diagnosis sequencially as required
+    complete_sort(order_patient_files, end_day_diagnosis)   
+    
     
